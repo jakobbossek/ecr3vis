3699bb6f327385a1920c94f890821b1ccff6ee89
Jakob Bossek <info@jakobbossek.de>
2021-06-01 10:40:33 +0200

    add docs for R{1,2,3}-indicators

--- R/moo_r.R
+++ R/moo_r.R
@@ -1,8 +1,67 @@
 #' @title
-#' R{1,2,3}-indicators
+#' R1, R2 and R3-indicator
 #'
 #' @description
-#' ...
+#' Consider two Pareto-front (approximation) sets
+#' \eqn{X = \{x_1, \ldots, x_{|X|}\}} and \eqn{Y = \{y_1, \ldots, y_{|Y|}\}} for
+#' some optimization problem with \eqn{m} objectives. Furthermore, let \eqn{U} be
+#' a set of \emph{utility functions}
+#' each of the form \eqn{u : R^m \to R} which maps each point in the objective
+#' space into a so-called measure of utility. Let \eqn{p} be a probability
+#' distribution over \eqn{U}. We denote by \eqn{u^{*}(A) = \max_{x \in A} u(x)}
+#' the maximum value reached by some utility function \eqn{u} on a set \eqn{A}.
+#'
+#' The R1-indicator measures the probability that approximation set \eqn{X} is
+#' better than approximation set \eqn{Y} by integrating over the all utility
+#' function \eqn{u \in U}. More precisely, it is defined as
+#' \deqn{
+#'   R1(X, Y, U, p) = \int_{u \in U} C(X, Y, u)p(x)du
+#' }
+#' where \eqn{C(X, Y, u) = 1} if \eqn{u^{*}(X) > u^{*}(Y)}, \eqn{C(X, Y, u) = 1/2}
+#' if \eqn{u^{*}(X)=u^{*}(Y)} and \eqn{C(X, Y, u) = 0} otherwise is termed the
+#' \emph{outcome function}.
+#'
+#' Interpretation: if \eqn{R1(X, Y, U, p) > 0.5}, \eqn{X} is better than \eqn{Y}.
+#' Owing to the definition it holds that
+#' \deqn{
+#'   R1(X, Y, U, p) = 1 - R1(X, Y, U, p)
+#' }
+#' and therefore \eqn{X} and \eqn{Y} cannot outperform each other simultaneously.
+#'
+#' In practice this defintion is not of much use. Therefore, a finite discrete
+#' set of utility functions is used with the uniform probability distribution.
+#' Then, \eqn{R1} can be written as
+#' \deqn{
+#'   R1(X, Y) = \frac{1}{|U|}\sum_{u \in U} C(X, Y, u).
+#' }
+#' The same approach is done for \eqn{R2} and \eqn{R3} which will be discussed in the
+#' following.
+#'
+#' Classical utility functions involve \emph{weighted sum}, \emph{Tschebycheff} and \emph{augmented
+#' Tschbycheff} all of which are availabe via the argument \code{utility} in the
+#' implementation. As an example, the set of (weighted) Tschebycheff utility
+#' functions is \eqn{U_{\infty} = (u_{\lambda})} where
+#' \deqn{
+#'   u_{\lambda}(x) = -\max_{j=1, \ldots, m} \left(\lambda_j \cdot |x_j - r_j|\right)
+#' }
+#' where \eqn{r \in R^m} is the ideal point or an approximation thereof and
+#' \eqn{\lambda \in R^m} is a weight vector with \eqn{\lambda_j \geq 0} for \eqn{j=1,\ldots,m}
+#' and \eqn{\sum_{j=1}^{m} \lambda_j = 1}. Further details are beyond the scope
+#' of this documentation (see [1] for in-depth information).
+#'
+#' The R2-indicator instead considers the expected utility values. It is defined as
+#' \deqn{
+#'   R2(X, Y, U, p) = E(u^{*}(X)) - E(u^{*}(Y)) = \int_{u \in U} (u^{*}(A) - u^{*}(B))p(u)du.
+#' }
+#' Interpretation: here, since the measure is defined as the difference of
+#' expectations, approximation set \eqn{X} is considered better than set \eqn{Y}
+#' if \eqn{R(X, Y) > 0}.
+#'
+#' Eventually, indicator R3 considers the ratios of best utility function values:
+#' \deqn{
+#'   R3(X, Y, U, p) = E\left(\frac{u^{*}(Y) - u^{*}(X)}{u^{*}(Y)}\right) = \int_{u \in U} \frac{u^{*}(Y) - u^{*}(X)}{u^{*}(Y)}p(u)du.
+#' }
+#' For further details we refer the reader to Hansen and Jaszkiewicz [1].
 #'
 #' @references
 #' [1] M. P. Hansen and A. Jaszkiewicz. 1998. Evaluating the quality of
@@ -10,9 +69,9 @@
 #' Mathematical Modeling, Technical University of Denmark.
 #'
 #' @param x [\code{matrix}]\cr
-#'   Point set in column major format.
+#'   First point set in column major format.
 #' @param y [\code{matrix}]\cr
-#'   Reference point set in column major format.
+#'   Second point set in column major format.
 #' @param ip [\code{numeric}]\cr
 #'   The utopia point of the true Pareto-front, i.e., each component of the point
 #'   contains the best value if the other objectives are neglected.
@@ -27,6 +86,7 @@
 #' @param utility [\code{character(1)}]\cr
 #'   Name of the utility function to use. Must be one of \dQuote{weighted-sum},
 #'   \dQuote{tschebycheff} or \dQuote{augmented-tschbycheff}.
+#'   Default is \dQuote{tschebycheff}.
 #' @template arg_dots_not_used
 #' @keywords optimize
 #' @family mootools
